126abfa87be1e65f1bbbaa6d853ec0723810c9c1
Sean Monaghan <smonagha@conncoll.edu>
2021-04-23 17:00:27 -0400

    got the homepage to render

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -4,12 +4,12 @@ const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
   try {
-    const newProject = await Project.create({
+    const newPost = await Blogpost.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(newPost);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -17,15 +17,15 @@ router.post('/', withAuth, async (req, res) => {
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const projectData = await Project.destroy({
+    const blogpostData = await Blogpost.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+    if (!blogpostData) {
+      res.status(404).json({ message: 'No blog post found with this id!' });
       return;
     }
 
